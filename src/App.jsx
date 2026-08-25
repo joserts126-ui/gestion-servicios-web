43444026e0ddc21b7389692150674bb9c0e8dc9e
joserts126 <joserts126@gmail.com>
2026-08-25 03:16:31 +0000

    fix: arreglo en app.js import versel

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Dashboard from './pages/Dashboard'
 import Servicios from './pages/Servicios'
 import Cotizaciones from './pages/Cotizaciones' // 1. Importamos la nueva pantalla
 import Proveedores from './pages/Proveedores'
-import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 function App() {
   return (
